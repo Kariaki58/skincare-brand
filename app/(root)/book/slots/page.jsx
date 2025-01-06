c8cf374888a6a85e0675eb0ffac2ae28f2e3cbd2
Kariaki58 <kariakistephen809@gmail.com>
2025-01-06 02:41:42 +0100

    removed calendar

--- app/(root)/book/slots/page.jsx
+++ app/(root)/book/slots/page.jsx
@@ -1,17 +1,8 @@
-"use client";
-import * as React from "react";
-import { Calendar } from "@/components/ui/calendar";
-
-
 export default function page() {
-    const [date, setDate] = React.useState(new Date())
-    console.log(date);
     return (
-        <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border shadow"
-        />
+        <div>
+            Page
+        </div>
+
     )
 }
