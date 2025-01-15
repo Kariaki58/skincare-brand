ded355634a6353ac1f1b9c6eb3f060a7c42c17f0
Kariaki58 <kariakistephen809@gmail.com>
2025-01-15 09:02:38 +0100

    wrapping customer table into suspense in react

--- app/dashboard/admin/customers/page.jsx
+++ app/dashboard/admin/customers/page.jsx
@@ -1,12 +1,15 @@
 import CustomerTable from "@/components/dashboard/admin/customers/customer-table";
 import { SidebarInsetComponent } from "@/components/dashboard/admin/side-bar-inset-component";
 import { SidebarInset } from "@/components/ui/sidebar";
+import { Suspense } from "react";
 
 export default function Customers() {
     return (
         <SidebarInset>
             <SidebarInsetComponent />
-            <CustomerTable />
+            <Suspense>
+                <CustomerTable />
+            </Suspense>
         </SidebarInset>
     );
 }
