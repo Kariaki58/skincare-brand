8b6c5b4545584172f2b41564ead37dc5be961799
Kariaki58 <kariakistephen809@gmail.com>
2025-01-18 18:33:20 +0100

    testing database integration

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -6,15 +6,24 @@ import Review from "@/components/app-ui/Reviews";
 import ClientShowCase from "@/components/app-ui/ClientShowCase";
 
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch('https://skincare-brand.vercel.app/api/products')
+  const posts = await data.json()
+  console.log(posts)
+  console.log(Array.isArray(posts))
   return (
     <div>
-      <Header />
+      <ul>
+      {posts.data.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+      </ul>
+      {/* <Header />
       <SectionHeadline />
       <ServiceSection />
       <CategorySection />
       <Review />
-      <ClientShowCase />
+      <ClientShowCase /> */}
     </div>
   );
 }
