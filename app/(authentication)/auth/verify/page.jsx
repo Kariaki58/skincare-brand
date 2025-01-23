e325df870ed3a62b8b6a71437c5343232a5fcd42
Kariaki58 <kariakistephen809@gmail.com>
2025-01-23 06:19:04 +0100

    added a suspense to protect usesearch params

--- app/(authentication)/auth/verify/page.jsx
+++ app/(authentication)/auth/verify/page.jsx
@@ -1,5 +1,5 @@
 import VerifyLoader from "@/components/app-ui/authentication/verify-loader"
-
+import { Suspense } from "react"
 
 export default async function Page() {
     return (
@@ -8,7 +8,9 @@ export default async function Page() {
                 <div>
                     <h1 className="text-2xl text-gray-700 font-medium text-center">Verifying Email</h1>
                     <div className="flex justify-center">
-                        <VerifyLoader />
+                        <Suspense fallback={<div className="text-gray-500 text-sm">Loading...</div>}>
+                            <VerifyLoader />
+                        </Suspense>
                     </div>
                 </div>
             </div>
