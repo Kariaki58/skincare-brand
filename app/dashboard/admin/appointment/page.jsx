934ceee4417938a84073bfc275a9431928115f23
Kariaki58 <kariakistephen809@gmail.com>
2025-02-14 05:40:07 +0100

    added usesearch params to a suspense

--- app/dashboard/admin/appointment/page.jsx
+++ app/dashboard/admin/appointment/page.jsx
@@ -2,6 +2,7 @@ import ServiceAdd from "./service-add"
 import ServiceDisplay from "./service-display"
 import { SidebarInsetComponent } from "@/components/dashboard/admin/side-bar-inset-component";
 import { SidebarInset } from "@/components/ui/sidebar";
+import { Suspense } from "react";
 
 
 export default function Page() {
@@ -9,7 +10,9 @@ export default function Page() {
         <SidebarInset>
             <SidebarInsetComponent />
             <div>
-                <ServiceAdd />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <ServiceAdd />
+                </Suspense>
                 <ServiceDisplay />
             </div>
         </SidebarInset>
