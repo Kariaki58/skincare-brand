12970ae230a64f6c81deb9a57d0e2edadb897e20
Kariaki58 <kariakistephen809@gmail.com>
2025-02-22 18:41:23 +0100

    modified

--- components/app-ui/footer-design/FooterWorkingHours.jsx
+++ components/app-ui/footer-design/FooterWorkingHours.jsx
@@ -9,22 +9,22 @@ export default function FooterWorkingHours() {
                     <PhoneCall size={42} className="text-white"/>
                     <div>
                         <h2 className="uppercase font-medium tracking-widest text-base mb-2">HAVE QUESTION?</h2>
-                        <p className="text-sm">Call Us at <span>704-713-8472</span></p>
+                        <p className="text-base">Call Us at <span>704-713-8472</span></p>
                     </div>
                 </div>
                 <div className="flex gap-3">
                     <MapPin size={42} className="text-white"/>
                     <div>
                         <h2 className="uppercase font-medium tracking-widest text-base mb-2">LOCATED IN</h2>
-                        <p className="text-sm">Visit our location at <span>5811 Freedom Drive, Charlotte, NC</span></p>
+                        <p className="text-base">Visit our location at <span>5811 Freedom Drive, Charlotte, NC</span></p>
                     </div>
                 </div>
                 <div className="flex gap-3">
                     <CalendarClock size={42} className="text-white"/>
                     <div>
                         <h2 className="uppercase font-medium tracking-widest text-base mb-2">business hours</h2>
-                        <p className="text-sm">Monday - Saturday: <span>8:00 AM - 6:00 PM</span></p>
-                        <p className="text-sm">Sunday: <span>Closed</span></p>
+                        <p className="text-base">Monday - Saturday: <span>8:00 AM - 6:00 PM</span></p>
+                        <p className="text-base">Sunday: <span>Closed</span></p>
                     </div>
                 </div>
             </div>
